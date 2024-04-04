6386101b27665032076edd62e996535493ca84e6
andvij <108976456+andvij@users.noreply.github.com>
2024-04-04 13:29:15 +0530

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -1,4 +1,14 @@
 class Father {
+  int x = 100;
 }
 class Son extends Father {
+  void disp() {
+    System.out.println("Get out of my House");
+  }
+}
+class Real {
+  public static void main(String[] args) {
+    Son s1 = new Son();
+    s1.disp();
+    System.out.println(s1.x);
 }
